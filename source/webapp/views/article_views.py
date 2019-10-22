4982191ae53f592e1fdf42aa324ca24b8133b7c1
Evgenii <lipatowqevgeniy@gmail.com>
2019-10-22 10:23:25 -0400

    Added search by tags

--- source/webapp/views/article_views.py
+++ source/webapp/views/article_views.py
@@ -30,6 +30,7 @@ class IndexView(ListView):
             queryset = queryset.filter(
                 Q(title__icontains=self.search_value)
                 | Q(author__icontains=self.search_value)
+                | Q(tags__name__iexact=self.search_value)
             )
         return queryset
 
